00d50640d04a43772919d68ee559bd999c79fee5
Grant Hernandez <grant.h.hernandez@gmail.com>
2015-06-30 14:24:57 -0400

    Fixed SPI speed for NRF as it was too fast

--- MEGA32A_SPI_LIBRARY/MEGA32A_SPI_LIBRARY.cpp
+++ MEGA32A_SPI_LIBRARY/MEGA32A_SPI_LIBRARY.cpp
@@ -22,11 +22,11 @@ void InitSPI(void) {
 	(0<<SPIE)|              // SPI Interrupt Enable
 	(0<<DORD)|              // Data Order (0:MSB first / 1:LSB first)
 	(1<<MSTR)|              // Master/Slave select
-	(0<<SPR1)|(0<<SPR0)|    // SPI Clock Rate
+	(1<<SPR1)|(0<<SPR0)|    // SPI Clock Rate (clk/64)
 	(0<<CPOL)|              // Clock Polarity (0:SCK low / 1:SCK hi when idle)
 	(0<<CPHA));             // Clock Phase (0:leading / 1:trailing edge sampling)
 	
-	SPSR |= (1<<SPI2X); // operate at clk/2
+	SPSR |= (0<<SPI2X); // operate at clk*2
 }
 
 //Sends and receives a byte through SPI
